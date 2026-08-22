88939551d22441babf2be678da1a51b223b05a54
Kawinpob Panbapeaw <Rungapa_jeamjit@hotmail.com>
2026-08-22 19:25:32 +1000

    home page ui

--- NoteLock/src/main.jsx
+++ NoteLock/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< HEAD:NoteLock/src/main.jsx
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './AuthContext.jsx'
@@ -9,5 +10,12 @@ createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
     </AuthProvider>
+=======
+import Homepage from './Homepage.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Homepage />
+>>>>>>> abb0e87 (home page ui):Note Lock/src/main.jsx
   </StrictMode>,
 )
